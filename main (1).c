912d639607172c24950d5990b3330ab92ed796ea
ankitraina <ankitraina1997@gmail.com>
2019-09-30 23:48:34 +0530

    Call by reference proram

---
+++ main (1).c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void swap(int* x , int* y);
+int main()
+{
+   int a, b;
+   printf("Enter the value of a");
+   scanf("%d",&a);
+   printf("Enter the value of b");
+   scanf("%d",&b);
+   printf("The values of a & b before swapping are: %d %d \n",a,b);
+   swap(&a,&b);
+   printf("The values of a & b after swapping are: %d %d",a,b);
+
+    return 0;
+}
+swap(int *x, int*y){
+    
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+    return;
+}
+
